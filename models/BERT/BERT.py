8105b784a917473caed9c27e63bc367936153d4b
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-07-05 15:52:43 +0100

    Update BERT.py

--- models/BERT/BERT.py
+++ models/BERT/BERT.py
@@ -364,85 +364,10 @@ class BertModel(nn.Module):
         return sequence_output, pooled_output
 
 
-class ScaledDotProductAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
-
-    def __init__(self, temperature, attn_dropout=0.1):
-        super().__init__()
-        self.temperature = temperature
-        self.dropout = nn.Dropout(attn_dropout)
-
-    def forward(self, q, k, v, mask=None):
-
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
-
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
-
-        attn = self.dropout(F.softmax(attn, dim=-1))
-        output = torch.matmul(attn, v)
-
-        return output, attn
-
-class MultiHeadAttention(nn.Module):
-    ''' Multi-Head Attention module '''
-
-    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
-        super().__init__()
-
-        self.n_head = n_head
-        self.d_k = d_k
-        self.d_v = d_v
-
-        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
-        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
-        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
-        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
-
-        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
-
-        self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
-
-
-    def forward(self, q, k, v, mask=None):
-
-        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
-        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
-
-        residual = q
-
-        # Pass through the pre-attention projection: b x lq x (n*dv)
-        # Separate different heads: b x lq x n x dv
-        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
-        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
-        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
-
-        # Transpose for attention dot product: b x n x lq x dv
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-
-        if mask is not None:
-            mask = mask.unsqueeze(1)   # For head axis broadcasting.
-
-        q, attn = self.attention(q, k, v, mask=mask)
-
-        # Transpose to move the head dimension back: b x lq x n x dv
-        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
-        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
-        q = self.dropout(self.fc(q))
-        q += residual
-
-        q = self.layer_norm(q)
-
-        return q, attn
-
-
 
 class BERT_model(nn.Module):
     def __init__(self, config, num_labels, activation='relu'):
-        super(TUCOREGCN_BERT, self).__init__()
         self.bert = BertModel(config)
-
         if activation == 'tanh':
             self.activation = nn.Tanh()
         elif activation == 'relu':
@@ -451,8 +376,8 @@ class BERT_model(nn.Module):
             assert 1 == 2, "you should provide activation function."
 
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size*3*(self.gcn_layers+1), self.num_labels)
-
+        self.classifier = nn.Linear(config.hidden_size, self.num_labels)
+        
         def init_weights(module):
             if isinstance(module, (nn.Linear, nn.Embedding)):
                 # Slightly different from the TF version which uses truncated_normal for initialization
@@ -463,80 +388,17 @@ class BERT_model(nn.Module):
                 module.gamma.data.normal_(mean=0.0, std=config.initializer_range)
             if isinstance(module, nn.Linear):
                 module.bias.data.zero_()
+        
         self.apply(init_weights)
 
-        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
-        self.turnAttention = MultiHeadAttention(config.num_attention_heads, config.hidden_size, self.attention_head_size, self.attention_head_size, config.attention_probs_dropout_prob)
-
-        rel_name_lists = ['speaker', 'dialog', 'entity']
         
     def forward(self, input_ids, token_type_ids, attention_mask, speaker_ids, graphs, mention_id, labels=None, turn_mask=None):
-        slen = input_ids.size(1)
-        sequence_outputs, pooled_outputs = self.bert(input_ids, speaker_ids, token_type_ids, attention_mask) 
-        
-        features = None
-        sequence_outputs, _ = self.turnAttention(sequence_outputs, sequence_outputs, sequence_outputs, turn_mask)
+        outputs = self.bert(input_ids, speaker_ids, token_type_ids, attention_mask) 
+      
+        pooled_output = outputs[1]
 
-        num_batch_turn = []
-
-        for i in range(len(graphs)): 
-            sequence_output = sequence_outputs[i]  
-            mention_num = torch.max(mention_id[i])  
-            num_batch_turn.append(mention_num+1)
-            mention_index = get_cuda(
-                (torch.arange(mention_num) + 1).unsqueeze(1).expand(-1, slen))  
-            mentions = mention_id[i].unsqueeze(0).expand(mention_num, -1)  
-            select_metrix = (mention_index == mentions).float()  
-            word_total_numbers = torch.sum(select_metrix, dim=-1).unsqueeze(-1).expand(-1, slen)  
-            select_metrix = torch.where(word_total_numbers > 0, select_metrix / word_total_numbers, select_metrix) 
-
-            x = torch.mm(select_metrix, sequence_output)  
-            x = torch.cat((pooled_outputs[i].unsqueeze(0), x), dim=0) 
-
-            if features is None:
-                features = x
-            else:
-                features = torch.cat((features, x), dim=0)
-
-        graph_big = dgl.batch(graphs)
-        output_features = [features]
-
-        for layer_num, GCN_layer in enumerate(self.GCN_layers):
-            start = 0
-            new_features = []
-            for idx in num_batch_turn:
-                new_features.append(features[start])
-                lstm_out = self.LSTM_layers[layer_num](features[start+1:start+idx-2].unsqueeze(0))
-                new_features += lstm_out
-                new_features.append(features[start+idx-2])
-                new_features.append(features[start+idx-1])
-                start += idx
-            features = torch.stack(new_features)
-            features = GCN_layer(graph_big, {"node": features})["node"]  
-            output_features.append(features)
-
-        graphs = dgl.unbatch(graph_big)
-
-        graph_output = list() 
-
-        fea_idx = 0
-        for i in range(len(graphs)):
-            node_num = graphs[i].number_of_nodes('node')
-            intergrated_output = None
-            for j in range(self.gcn_layers + 1):
-                if intergrated_output == None:
-                    intergrated_output = output_features[j][fea_idx]
-                else:
-                    intergrated_output = torch.cat((intergrated_output, output_features[j][fea_idx]), dim=-1)
-                intergrated_output = torch.cat((intergrated_output, output_features[j][fea_idx + node_num - 2]), dim=-1)
-                intergrated_output = torch.cat((intergrated_output, output_features[j][fea_idx + node_num - 1]), dim=-1)
-            fea_idx += node_num
-            graph_output.append(intergrated_output)
-        graph_output = torch.stack(graph_output)
-
-        pooled_output = self.dropout(graph_output)
+        pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-        logits = logits.view(-1, self.num_labels)
 
         if labels is not None:
             loss_fct = BCEWithLogitsLoss()
